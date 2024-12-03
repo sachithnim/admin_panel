f78db5ba4189b3592e6de54a75b845bdaa153043
sachithnim <nimendras07@gmail.com>
2024-12-04 02:01:51 +0530

    welcome page updated

--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,20 @@ Route::middleware([
     'verified',
 ])->group(function () {
     Route::get('/dashboard', [ProductController::class, 'index'])->name('dashboard');
+    Route::get('/add-product', [ProductController::class, 'create'])->name('add-product');
+    Route::post('/add-product', [ProductController::class, 'store'])->name('store-product');
+    Route::get('/edit-product/{product}', [ProductController::class, 'edit'])->name('edit-product');
+    Route::put('/edit-product/{product}', [ProductController::class, 'update'])->name('update-product');
+    Route::delete('/delete-product/{product}', [ProductController::class, 'destroy'])->name('delete-product');
+
+
+    Route::get('/category', [CategoryController::class, 'index'])->name('category');
+    Route::get('/add-category', [CategoryController::class, 'create'])->name('add-category');
+    Route::post('/add-category', [CategoryController::class, 'store'])->name('store-category');
+
+    Route::get('/edit-category/{category}', [CategoryController::class, 'edit'])->name('edit-category');
+    Route::put('/edit-category/{category}', [CategoryController::class, 'update'])->name('update-category');
+    Route::delete('/delete-category/{category}', [CategoryController::class, 'destroy'])->name('delete-category');
 });
 
-Route::get('/add-product', [ProductController::class, 'create'])->name('add-product');
-Route::post('/add-product', [ProductController::class, 'store'])->name('store-product');
-Route::get('/edit-product/{product}', [ProductController::class, 'edit'])->name('edit-product');
-Route::put('/edit-product/{product}', [ProductController::class, 'update'])->name('update-product');
-Route::delete('/delete-product/{product}', [ProductController::class, 'destroy'])->name('delete-product');
 
-
-Route::get('/category', [CategoryController::class, 'index'])->name('category');
-Route::get('/add-category', [CategoryController::class, 'create'])->name('add-category');
-Route::post('/add-category', [CategoryController::class, 'store'])->name('store-category');
-
-Route::get('/edit-category/{category}', [CategoryController::class, 'edit'])->name('edit-category');
-Route::put('/edit-category/{category}', [CategoryController::class, 'update'])->name('update-category');
-Route::delete('/delete-category/{category}', [CategoryController::class, 'destroy'])->name('delete-category');
