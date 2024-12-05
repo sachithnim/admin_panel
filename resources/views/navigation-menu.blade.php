4ee527eb7e3b2ea70752933c1d0848b719d7642d
sachithnim <nimendras07@gmail.com>
2024-12-05 15:55:13 +0530

    add logo to nav bar

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-mark class="block h-9 w-auto" />
+                        <img src="{{ asset('images/logo_black.png') }}" alt="Logo" class="img-fluid" style="width: 100px; height: auto;">
                     </a>
                 </div>
 
